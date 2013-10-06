f4508b04f17ff8e40d4f2c769fad865619af1c1a
Lim Chee Kin <limcheekin@vobject.com>
2013-10-06 13:40:53 +0800

    update activiti to 5.13 and excludes jars from dependencies

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -10,17 +10,22 @@ grails.project.dependency.resolution = {
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-		compile ('org.activiti:activiti-engine:5.12.1') {
-			excludes 'livetribe-jsr223'
+		compile ('org.activiti:activiti-engine:5.13') {
+			excludes 'livetribe-jsr223', 'spring-beans'
 		}
-		runtime 'org.activiti:activiti-spring:5.12.1'
+		runtime ('org.activiti:activiti-spring:5.13') {
+			excludes 'spring-context', 'spring-jdbc', 'spring-orm', 'slf4j-log4j12', 'commons-dbcp'
+		}
+		//runtime 'org.springframework:spring-asm:3.1.4.RELEASE'
 		runtime 'javax.mail:mail:1.4.1'
-		test 'org.subethamail:subethasmtp-smtp:1.2'
-		test 'org.subethamail:subethasmtp-wiser:1.2'
+		test ('org.subethamail:subethasmtp-smtp:1.2') {
+			excludes 'commons-logging'
+		}
+		test ('org.subethamail:subethasmtp-wiser:1.2') {
+			excludes 'commons-logging'
+		}
     }
 	plugins {
-		build ':release:2.2.1', ':rest-client-builder:1.0.3', {
-            export = false
-        }
+		build ":release:3.0.1"
 	}
 }
